d7fee28acfef20e7f25c90be3120c1cec4c0b66b
Valentin Lageard <valentin.lageard.pro@gmail.com>
2020-01-13 16:01:10 +0100

    '(null)' supported if precision allows it

--- format_str.c
+++ format_str.c
@@ -6,7 +6,7 @@
 /*   By: vlageard <vlageard@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/01/08 14:58:16 by vlageard          #+#    #+#             */
-/*   Updated: 2020/01/12 18:15:58 by vlageard         ###   ########.fr       */
+/*   Updated: 2020/01/13 15:57:25 by vlageard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,11 +21,13 @@ int		str_get_str_size(t_format *format, char *va_str)
 	int	va_strlen;
 	
 	if (!va_str)
-		va_strlen = 0;
+		va_strlen = format->precision >= 6 || format->precision == -1 ? 6 : 0;
 	else
 		va_strlen = ft_strlen(va_str);
 	if (format->precision == -1)
+	{
 		str_size = va_strlen + ft_max(0, format->fieldwidth - va_strlen);
+	}
 	else
 	{
 		str_size = ft_min(va_strlen, format->precision) +
@@ -61,6 +63,28 @@ char	*str_fill_str(char *str, t_format *format, char *va_str)
 	return (str);
 }
 
+char	*str_fill_null(char *str, t_format *format)
+{
+	int	pad;
+	char *null;
+	
+	pad = ft_max(0, format->fieldwidth - 6);
+	null = ft_strdup("(null)");
+	if (format->fieldwidth_mode != 2)
+	{
+		ft_memset((void *)str, 32, pad);
+		ft_memcpy((void *)(str + pad), (void *)null, 6);
+	}
+	else
+	{
+		ft_memcpy((void *)str, (void *)null, 6);
+		ft_memset((void *)(str + 6), 32, pad);
+
+	}
+	free(null);
+	return (str);
+}
+
 char	*format_str(t_format *format, va_list valist)
 {
 	char	*str;
@@ -71,7 +95,10 @@ char	*format_str(t_format *format, va_list valist)
 	str_size = str_get_str_size(format, va_str);
 	if (!(str = (char *)malloc(sizeof(char) * (str_size + 1))))
 		return (NULL);
-	str = str_fill_str(str, format, va_str);
+	if (!va_str && (format->precision >= 6 || format->precision == -1))
+		str = str_fill_null(str, format);
+	else
+		str = str_fill_str(str, format, va_str);
 	str[str_size] = 0;
 	return (str);
 }
