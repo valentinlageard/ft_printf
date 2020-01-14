51d2ebd9cc7cc6b2e5620563c66d31d761f82505
Valentin Lageard <valentin.lageard.pro@gmail.com>
2020-01-14 19:10:19 +0100

    weird case better handling

--- format_validity.c
+++ format_validity.c
@@ -6,7 +6,7 @@
 /*   By: vlageard <vlageard@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/01/14 14:40:04 by vlageard          #+#    #+#             */
-/*   Updated: 2020/01/14 19:03:57 by vlageard         ###   ########.fr       */
+/*   Updated: 2020/01/14 19:09:29 by vlageard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,14 +30,14 @@ void check_padding(int *j, const char *fstr)
 		(*j)++;
 		if (fstr[*j] == '*')
 			(*j)++;
+		else if (fstr[*j] == '0' && fstr[*j + 1] == '*')
+			*j += 2;
 		else
 		{
 			while (ft_isdigit(fstr[*j]))
 				(*j)++;
 		}
 	}
-	if (fstr[*j] == '*')
-		(*j)++;
 }
 
 void check_precision(int *j, const char *fstr)
@@ -60,7 +60,7 @@ int	format_is_valid(int i, const char *fstr)
 	int	j;
 	
 	j = i;
-	check_padding(&j, fstr); // This needs to take into account that if fw is 0, then it is a malformed format
+	check_padding(&j, fstr);
 	check_precision(&j, fstr);
 	if (is_conversion(fstr[j]) || fstr[j] == '%')
 		return (1);
